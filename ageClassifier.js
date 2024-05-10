37c4a2eadc88fcd4c9211f7483ffded5448a1b07
Ashimelis <shimelisget2@gmail.com>
2024-05-10 04:45:28 -0700

    Age classifier asignment of shimelis

---
+++ ageClassifier.js
@@ -0,0 +1,13 @@
+
+
+const age = 30;
+//prompt();
+if (age < 13) {
+  console.log(" You are Child");
+}else if (age >= 13 && age <= 19 ) {
+  console.log("You are Teenager");
+} else if (age >= 20 && age <= 59 ){
+    console.log("You are Adult");
+} else if (age > 60) {
+    console.log("You are Senior")
+}
