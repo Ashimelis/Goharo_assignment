6797d427de1ae85c85401991bc85097aaaba94fb
Ashimelis <shimelisget2@gmail.com>
2024-05-10 04:28:23 -0700

    Temprture conversion

---
+++ temperture.Shimelis.js
@@ -0,0 +1,20 @@
+
+
+
+function celcToFahr( n ) {
+    return ((n * 9.0 / 5.0) + 32.0);
+}
+ 
+// Driver code
+const n = 20;
+ 
+console.log(celcToFahr( n ));
+
+function fahrTocelc( m ) {
+    return ((m - 32) * 5/9);
+}
+ 
+// Driver code
+const m = 20;
+ 
+console.log(fahrToCelc( m));
