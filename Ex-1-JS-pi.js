f1ee7799f9189763aa1789dfc960cf0d5c77b110
Ashimelis <shimelisget2@gmail.com>
2024-05-10 01:18:03 -0700

    peronal infomation

---
+++ Ex-1-JS-pi.js
@@ -0,0 +1,12 @@
+let firstName= "Shimelis";
+let middleName= "Geathun";
+let lastName= "Ayele";
+let age=43;
+let homeTown="Arba Minch";
+let color="purple";
+let isStudent=true;
+console.log(firstName+middleName+lastName);
+console.log(age);
+console.log(homeTown)
+console.log(color);
+console.log(isStudent);
